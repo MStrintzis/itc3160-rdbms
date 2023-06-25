924a3b68c1755fd1721d4b3ebb952c57dcbb82f9
MStrintzis <134636890+MStrintzis@users.noreply.github.com>
2023-06-25 14:46:47 +0300

    Coursework Final Update

--- question_11.sql
+++ question_11.sql
@@ -4,15 +4,15 @@ INSERT INTO Worker (empId,lastName,firstName,departmentId,birthDate,hireDate,sal
 VALUES (267,'Strintzis','Michael',2,'02-APR-99','26-JUN-23',90000) ;
 
 INSERT INTO Worker (empId,lastName,firstName,departmentId,birthDate,hireDate,salary)
-VALUES (264,'Bastanis','Dimitris',2,'08-FEB-00','26-JUN-23',85000) ;
+VALUES (243,'Bastanis','Dimitris',2,'08-FEB-01','26-JUN-23',85000) ;
 
 INSERT INTO Assign (projNo,empId,hoursAssigned,rating)
 VALUES (1001,267,25,5) ;
 INSERT INTO Assign (projNo,empId,hoursAssigned,rating)
 VALUES (1005,267,25,2) ;
 INSERT INTO Assign (projNo,empId,hoursAssigned,rating)
-VALUES (1031,264,15,5) ;
+VALUES (1031,2243,15,5) ;
 INSERT INTO Assign (projNo,empId,hoursAssigned,rating)
-VALUES (1043,264,30,3) ;
+VALUES (1043,243,30,3) ;
 
-SELECT * FROM ProjectEmployees  WHERE empId = '267' OR empId = '264';
+SELECT * FROM ProjectEmployees  WHERE empId = '267' OR empId = '243';
--- question_12.sql
+++ question_12.sql
@@ -3,3 +3,5 @@
 UPDATE Assign
 SET hoursAssigned = 30
 WHERE empId = '105' AND projNo = '1019' ;
+
+SELECT * FROM Assign WHERE empId = '105' ;
